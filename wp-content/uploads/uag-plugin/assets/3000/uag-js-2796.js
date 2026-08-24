e801371740d697a02fa4d0838f905d7034826588
Tamara <tamaradjurcik@gmail.com>
2026-08-24 15:57:25 +0200

    Fixed

--- wp-content/uploads/uag-plugin/assets/3000/uag-js-2796.js
+++ wp-content/uploads/uag-plugin/assets/3000/uag-js-2796.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
 	UAGBButtonChild.init( '.uagb-block-y1idttts' );
 });
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-zcxlxfpm' );
+});
  });
